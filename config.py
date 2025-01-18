c4a90a0bee02241bd950e35c84b53e809b68d5a2
emiranivala <taitufam@gmail.com>
2025-01-18 14:39:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "25276967"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "daf793293a5a244e5c426a129656e0a1"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1679112664"))
+OWNER_ID = int(getenv("OWNER_ID", "7446465090"))
 SUPPORT_GRP = "+cXIPgHSuJnxiNjU1"
 UPDATE_CHNL = "ai_image_junction"
 OWNER_USERNAME = "OfficialDurgesh"
